6521a7f37785dcdc14f421b0e057e375a858d93f
Ben Johnson <ben@bnjn.tech>
2023-02-15 11:08:03 +0000

    edge position tests now passing

--- modules/Cell.py
+++ modules/Cell.py
@@ -9,6 +9,7 @@ class Cell():
     def die(self) -> None:
         self.alive = False
 
+    # TODO: Refeactor this abomination.
     def check_living_neighbours(self, grid) -> int:
         for row in grid:
             for cell in row:
@@ -16,36 +17,89 @@ class Cell():
                     y = grid.index(row)
                     x = row.index(self)
                     neighbouring_cells = []
-                    try:
-                        neighbouring_cells.append(row[x + 1])
-                    except IndexError:
-                        pass
-                    try:
-                        neighbouring_cells.append(row[x - 1])
-                    except IndexError:
-                        pass
-                    try:
-                        neighbouring_cells.append(grid[y + 1][x])
-                    except IndexError:
-                        pass
-                    try:
+                    if x-1 and y-1 < 0:
+                        # Top left corner
+                        try:
+                            neighbouring_cells.append(row[x + 1])
+                        except IndexError:
+                            pass
+                        try:
+                            neighbouring_cells.append(grid[y + 1][x + 1])
+                        except IndexError:
+                            pass
+                        try:
+                            neighbouring_cells.append(grid[y + 1][x])
+                        except IndexError:
+                            pass
+                    elif x-1 < 0:
+                        # Left edge
+                        try:
+                            neighbouring_cells.append(grid[y - 1][x])
+                        except IndexError:
+                            pass
+                        try:
+                            neighbouring_cells.append(grid[y - 1][x + 1])
+                        except IndexError:
+                            pass
+                        try:
+                            neighbouring_cells.append(row[x + 1])
+                        except IndexError:
+                            pass
+                        try:
+                            neighbouring_cells.append(grid[y + 1][x])
+                        except IndexError:
+                            pass
+                        try:
+                            neighbouring_cells.append(grid[y + 1][x + 1])
+                        except IndexError:
+                            pass
+                    elif y-1 < 0:
+                        # Top edge
+                        try:
+                            neighbouring_cells.append(row[x - 1])
+                        except IndexError:
+                            pass
+                        try:
+                            neighbouring_cells.append(row[x + 1])
+                        except IndexError:
+                            pass
+                        try:
+                            neighbouring_cells.append(grid[y + 1][x])
+                        except IndexError:
+                            pass
+                        try:
+                            neighbouring_cells.append(grid[y + 1][x + 1])
+                        except IndexError:
+                            pass
+                        try:
+                            neighbouring_cells.append(grid[y - 1][x - 1])
+                        except IndexError:
+                            pass
+                    else:
+                        # Try to catch if on right edge
+                        try:
+                            neighbouring_cells.append(row[x + 1])
+                        except IndexError:
+                            pass                            
+                        try:
+                            neighbouring_cells.append(grid[y + 1][x])
+                        except IndexError:
+                            pass
+                        try:
+                            neighbouring_cells.append(grid[y + 1][x + 1])
+                        except IndexError:
+                            pass
+                        try:
+                            neighbouring_cells.append(grid[y - 1][x + 1])
+                        except IndexError:
+                            pass
+                        try:                            
+                            neighbouring_cells.append(grid[y + 1][x - 1])
+                        except IndexError:
+                            pass
+
                         neighbouring_cells.append(grid[y - 1][x])
-                    except IndexError:
-                        pass
-                    try:
-                        neighbouring_cells.append(grid[y + 1][x + 1])
-                    except IndexError:
-                        pass
-                    try:
+                        neighbouring_cells.append(row[x - 1])
                         neighbouring_cells.append(grid[y - 1][x - 1])
-                    except IndexError:
-                        pass
-                    try:
-                        neighbouring_cells.append(grid[y - 1][x + 1])
-                    except IndexError:
-                        pass
-                    try:
-                        neighbouring_cells.append(grid[y + 1][x - 1])
-                    except IndexError:
-                        pass
+                           
                     return len(Counter(neighbouring_cells)) - 1
