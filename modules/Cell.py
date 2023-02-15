f341828dc5621bf2d8fa34702e36bb3285bda40a
Ben Johnson <ben@bnjn.tech>
2023-02-15 10:03:39 +0000

    can now place cell and check for neighbours from non edge position

--- modules/Cell.py
+++ modules/Cell.py
@@ -1,7 +1,28 @@
+from collections import Counter
+
+
 class Cell():
     
     def __init__(self) -> None:
         self.alive = True
 
-    def die(self) -> bool:
+    def die(self) -> None:
         self.alive = False
+
+    def check_living_neighbours(self, grid) -> int:
+        for row in grid:
+            for cell in row:
+                if cell == self:
+                    y = grid.index(row)
+                    x = row.index(self)
+                    neighbouring_cells = [
+                        row[x + 1], # right
+                        row[x - 1], # left
+                        grid[y + 1][x], # down
+                        grid[y - 1][x], # up
+                        grid[y - 1][x - 1], # up left
+                        grid[y - 1][x + 1], # up right
+                        grid[y + 1][x - 1], # down left
+                        grid[y + 1][x + 1] # down right
+                    ]
+                    return len(Counter(neighbouring_cells)) - 1
--- tests/test_cell.py
+++ tests/test_cell.py
@@ -6,12 +6,39 @@ class TestCell(unittest.TestCase):
     def test_can_create_living_cell(self):
         new_cell = Cell()
         self.assertIsInstance(new_cell, Cell)
-        self.assertEquals(new_cell.alive, True)
+        self.assertEqual(new_cell.alive, True)
 
     def test_cell_can_die(self):
         new_cell = Cell()
         new_cell.die()
-        self.assertEquals(new_cell.alive, False)
+        self.assertEqual(new_cell.alive, False)
+    
+    def test_check_living_neighbours(self):
+        new_cell = Cell()
+        grid = [
+            [None, None, Cell()],
+            [None, new_cell, Cell()],
+            [None, Cell(), None]
+        ]
+        self.assertEqual(new_cell.check_living_neighbours(grid), 3)
+
+    def test_more_living_neighbours(self):
+        new_cell = Cell()
+        grid = [
+            [None, Cell(), Cell()],
+            [Cell(), new_cell, Cell()],
+            [Cell(), Cell(), None]
+        ]
+        self.assertEqual(new_cell.check_living_neighbours(grid), 6)
+
+    def test_edge_living_neighbours(self):
+        new_cell = Cell()
+        grid = [
+            [None, Cell(), Cell()],
+            [Cell(), Cell(), new_cell],
+            [Cell(), Cell(), None]
+        ]
+        self.assertEqual(new_cell.check_living_neighbours(grid), 4)
 
 if __name__ == '__main__':
     unittest.main()
